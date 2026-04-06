90da3e1918b13a315ee0e890a5eadf9e8a69ea5e
Johannsonmanson <91399377+JohannVasquez@users.noreply.github.com>
2026-04-06 01:23:10 -0400

    feat: add use cases with images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Navbar } from "@/components/landing/Navbar";
 import { Problem } from "@/components/landing/Problem";
 import { Showcase } from "@/components/landing/Showcase";
 import { Trust } from "@/components/landing/Trust";
+import { UseCases } from "@/components/landing/UseCases";
 import { WaitlistForm } from "@/components/landing/WaitlistForm";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <main>
         <Hero />
         <Problem />
+        <UseCases />
         <HowItWorks />
         <Showcase />
         <Trust />
